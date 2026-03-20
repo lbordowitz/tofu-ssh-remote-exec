06a81fc37422e9cae86ce0a12d4eae9acb30f5dd
Larry Bordowitz <laurence.bordowitz@gmail.com>
2026-03-20 17:40:36 -0500

    NB: might have to subscribe to debian to use ami
    Signed-off-by: Larry Bordowitz <laurence.bordowitz@gmail.com>

--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
+
+# you might have to subscribe to Debian 12 to use its AMI in the project:
+# https://aws.amazon.com/marketplace/pp/prodview-g5rooj5oqzrw4
 variable "ami" {
     default = "ami-0f3febef860d6b7f6"
     type = string
